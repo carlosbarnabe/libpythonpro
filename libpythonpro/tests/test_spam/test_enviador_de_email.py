7feae7fffae2e2ab75fa30a42605eac81ecd1a1b
carlosbarnabe <carlosbarnabedev@gmail.com>
2022-07-13 17:39:09 -0300

    Iniciado Estudo de Banco de Dados

--- libpythonpro/tests/test_spam/test_enviador_de_email.py
+++ libpythonpro/tests/test_spam/test_enviador_de_email.py
@@ -1,16 +1,39 @@
-from libpythonpro.spam.enviador_de_email import Enviador
+import pytest
+
+from libpythonpro.spam.enviador_de_email import Enviador, EmailInvalido
 
 
 def test_criar_enviador_de_email():
     enviador = Enviador()
     assert enviador is not None
 
-def test_remetente():
+
+@pytest.mark.parametrize(
+    'remetente',
+    ['carlosalvesab@gmail.com', 'foo@bar.com.br']
+)
+def test_remetente(remetente):
     enviador = Enviador()
     resultado = enviador.enviar(
-        'carlosalvesab@gmail.com',
+        remetente,
         'carlosbarnabedev@gmail.com',
         'Curso Marketing',
         'Abra esse email para saber mais!'
     )
-    assert 'carlosalvesab@gmail.com' in resultado
+    assert remetente in resultado
+
+
+@pytest.mark.parametrize(
+    'remetente',
+    ['', 'carlos']
+)
+
+def test_remetente_invalido(remetente):
+    enviador = Enviador()
+    with pytest.raises(EmailInvalido):
+        enviador.enviar(
+            remetente,
+            'carlosbarnabedev@gmail.com',
+            'Curso Marketing',
+            'Abra esse email para saber mais!'
+        )
