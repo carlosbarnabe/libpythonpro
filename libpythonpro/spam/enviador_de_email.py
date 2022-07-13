7bae9a43acb6a207782b18431d7c287b7c7208fa
carlosbarnabe <carlosbarnabedev@gmail.com>
2022-07-13 17:39:09 -0300

    Iniciado Estudo de Banco de Dados

--- libpythonpro/spam/enviador_de_email.py
+++ libpythonpro/spam/enviador_de_email.py
@@ -1,3 +1,8 @@
 class Enviador:
-    def enviar(self, rementente, destinatario, assunto, corpo):
-        return 'carlosalvesab@gmail.com'
+    def enviar(self, remetente, destinatario, assunto, corpo):
+        if '@' not in remetente:
+            raise EmailInvalido(f'Email Inválido {remetente}')
+        return remetente
+
+class EmailInvalido(Exception):
+    pass
